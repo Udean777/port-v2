8177d4be720110ec1a8b06128bfadbf9bcf25699
Udean777 <uaart123@gmail.com>
2023-06-23 17:47:16 +0700

    nambah konten

--- app/page.js
+++ app/page.js
@@ -2,10 +2,14 @@
 import Navbar from './components/Navbar'
 import Hero from './Hero'
 import './globals.css'
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-zinc-200 dark:bg-zinc-800">
+      <Head>
+        <meta property='og:image' content='/hero1.png'/>
+      </Head>
       <Navbar/>
 
       <Hero/>
